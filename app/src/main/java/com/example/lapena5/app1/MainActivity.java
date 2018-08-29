985effe3f834a2d0652a6c22ae25b27f10e33f1b
AlexLapena <alexflapena@gmail.com>
2018-08-28 22:22:23 -0400

    Add note creation activity

--- app/src/main/java/com/example/lapena5/app1/MainActivity.java
+++ app/src/main/java/com/example/lapena5/app1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.lapena5.app1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createNote(View view) {
+        Intent intent = new Intent(MainActivity.this, NotePadActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/lapena5/app1/NotePadActivity.java
@@ -0,0 +1,13 @@
+package com.example.lapena5.app1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotePadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_pad);
+    }
+}
